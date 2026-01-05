0c100e2a3bd33d81b13d8151ecf64e9f6e3a0145
r5b9t8zxq6-ctrl <r5b9t8zxq6@privaterelay.appleid.com>
2026-01-06 00:58:05 +0300

    config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+VEO3_API_KEY = os.getenv("VEO3_API_KEY")
+YOOKASSA_SHOP_ID = os.getenv("YOOKASSA_SHOP_ID")
+YOOKASSA_API_KEY = os.getenv("YOOKASSA_API_KEY")
+DOMAIN = os.getenv("DOMAIN")
+YOOMONEY_WALLET = os.getenv("YOOMONEY_WALLET")
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
